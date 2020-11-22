45699772c01478019892b88bdf3c851fec86fa0c
korrann <55925673+AbrolKaran@users.noreply.github.com>
2020-11-22 20:25:24 +0530

    Update SquareObstacle.java

--- sample/SquareObstacle.java
+++ sample/SquareObstacle.java
@@ -14,6 +14,8 @@ public class SquareObstacle extends Obstacle{
     private Rectangle r2;
     private Rectangle r3;
     private Rectangle r4;
+    private ArrayList<Rectangle> rect;
+
 
     @Override
     public void move(){
@@ -37,7 +39,7 @@ public class SquareObstacle extends Obstacle{
         r.getChildren().addAll(r1,r2,r3,r4);
     }
 
-    public SquareObstacle(int d, ArrayList<Integer> c,float x,float y,float l,int dir){
+    public SquareObstacle(int d, ArrayList<String> c,float x,float y,float l,int dir){
         super(d,c,x,y,l,dir);
         this.length = l;
         this.direction = dir;
@@ -46,28 +48,33 @@ public class SquareObstacle extends Obstacle{
         r1.setY(y);
         r1.setWidth(length);
         r1.setHeight(10);
-        r1.setFill(Color.BLUE);
+        r1.setFill(Color.web(colors.get(0)));
 
         r2 = new Rectangle();
         r2.setX(x);
         r2.setY(y);
         r2.setWidth(10);
         r2.setHeight(length);
-        r2.setFill(Color.GREEN);
+        r2.setFill(Color.web(colors.get(1)));
 
         r3 = new Rectangle();
         r3.setX(x);
         r3.setY(y+length);
         r3.setWidth(length);
         r3.setHeight(10);
-        r3.setFill(Color.YELLOW);
+        r3.setFill(Color.web(colors.get(2)));
 
         r4 = new Rectangle();
         r4.setX(x+length);
         r4.setY(y+10);
         r4.setWidth(10);
         r4.setHeight(length);
-        r4.setFill(Color.RED);
+        r4.setFill(Color.web(colors.get(3)));
+
+        rect = new ArrayList<>();
+        rect.add(r1);
+        rect.add(r2);
+        rect.add(r3);
 
     }
 
@@ -83,7 +90,5 @@ public class SquareObstacle extends Obstacle{
         return r3;
     }
 
-    public Rectangle getR4() {
-        return r4;
-    }
+    public Rectangle getR4() { return r4; }
 }
