83e563052d1c83d2bd8d945441c0aec6300acd51
korrann <55925673+AbrolKaran@users.noreply.github.com>
2020-11-22 20:27:12 +0530

    Update DoubleCircleObstacle.java

--- sample/DoubleCircleObstacle.java
+++ sample/DoubleCircleObstacle.java
@@ -36,7 +36,7 @@ public class DoubleCircleObstacle extends Obstacle{
         return c2;
     }
 
-    DoubleCircleObstacle(int d, ArrayList<Integer> c, float x, float y,float l,int dir){
+    DoubleCircleObstacle(int d, ArrayList<String> c, float x, float y,float l,int dir){
         super(d,c,x,y,l,dir);
         c1 = new CircleObstacle(d,c,x,y,l+20,1*dir);
         c2 = new CircleObstacle(d,c,x,y,l,-1*dir);
