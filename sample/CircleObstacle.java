776fd1dee5b373c37b65b8c11bca31ff4cba4222
korrann <55925673+AbrolKaran@users.noreply.github.com>
2020-11-22 18:01:15 +0530

    Update CircleObstacle.java

--- sample/CircleObstacle.java
+++ sample/CircleObstacle.java
@@ -1,4 +1,5 @@
 package sample;
+import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Arc;
 import javafx.scene.shape.ArcType;
@@ -12,6 +13,7 @@ public class CircleObstacle extends Obstacle{
     private Arc arc1;
     private Arc arc2;
     private Arc arc3;
+    private ArrayList<Arc> arcList;
 
     public Arc getArc4() {
         return arc4;
@@ -29,10 +31,14 @@ public class CircleObstacle extends Obstacle{
         return arc3;
     }
 
+    public ArrayList<Arc> getArcList() {
+        return arcList;
+    }
+
     @Override
     public void move(){
         Rotate rotate = new Rotate();
-        rotate.setAngle(1);
+        rotate.setAngle(1*direction);
         rotate.setPivotX(posX);
         rotate.setPivotY(posY);
         getArc1().getTransforms().add(rotate);
@@ -47,20 +53,21 @@ public class CircleObstacle extends Obstacle{
     }
 
     @Override
-    public void display(){
-
+    public void display(Group r){
+        r.getChildren().addAll(arc1,arc2,arc3,arc4);
     }
 
-    public CircleObstacle(int d, ArrayList<Integer> c,float x, float y){
+    public CircleObstacle(int d, ArrayList<Integer> c,float x, float y,float l, int dir){
 
-        super(d,c,x,y);
-        this.length = 45;
+        super(d,c,x,y,l,dir);
+        this.length = l;
+        this.direction = dir;
         arc1 = new Arc();
         arc1.setCenterX(posX);
         arc1.setCenterY(posY);
-        arc1.setStartAngle(90.0f);
-        arc1.setRadiusX(45);
-        arc1.setRadiusY(45);
+        arc1.setStartAngle(135);
+        arc1.setRadiusX(length);
+        arc1.setRadiusY(length);
         arc1.setLength(90.0f);
         arc1.setStroke(Color.GREEN);
         arc1.setStrokeWidth(10.0f);
@@ -70,9 +77,9 @@ public class CircleObstacle extends Obstacle{
         arc2 = new Arc();
         arc2.setCenterX(posX);
         arc2.setCenterY(posY);
-        arc2.setStartAngle(180.0f);
-        arc2.setRadiusX(45);
-        arc2.setRadiusY(45);
+        arc2.setStartAngle(225);
+        arc2.setRadiusX(length);
+        arc2.setRadiusY(length);
         arc2.setLength(90.0f);
         arc2.setStroke(Color.RED);
         arc2.setStrokeWidth(10.0f);
@@ -82,10 +89,10 @@ public class CircleObstacle extends Obstacle{
         arc3 = new Arc();
         arc3.setCenterX(posX);
         arc3.setCenterY(posY);
-        arc3.setStartAngle(270.0f);
-        arc3.setRadiusX(45);
-        arc3.setRadiusY(45);
-        arc3.setLength(90.0f);
+        arc3.setStartAngle(315);
+        arc3.setRadiusX(length);
+        arc3.setRadiusY(length);
+        arc3.setLength(90);
         arc3.setStroke(Color.YELLOW);
         arc3.setStrokeWidth(10.0f);
         arc3.setFill(Color.TRANSPARENT);
@@ -94,13 +101,19 @@ public class CircleObstacle extends Obstacle{
         arc4 = new Arc();
         arc4.setCenterX(posX);
         arc4.setCenterY(posY);
-        arc4.setStartAngle(0.0f);
-        arc4.setRadiusX(45);
-        arc4.setRadiusY(45);
+        arc4.setStartAngle(45);
+        arc4.setRadiusX(length);
+        arc4.setRadiusY(length);
         arc4.setLength(90.0f);
         arc4.setStroke(Color.BLUE);
         arc4.setStrokeWidth(10.0f);
         arc4.setFill(Color.TRANSPARENT);
         arc4.setType(ArcType.OPEN);
+
+        arcList = new ArrayList<>();
+        arcList.add(arc1);
+        arcList.add(arc2);
+        arcList.add(arc3);
+        arcList.add(arc4);
     }
 }
